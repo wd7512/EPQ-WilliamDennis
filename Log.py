7bc77bbcc682d8b3e63fdfe5e777f32dd7937f21
William Dennis <35834979+wd7512@users.noreply.github.com>
2018-01-31 09:12:37 +0000

    Update Log.py

--- Log.py
+++ Log.py
@@ -1,1 +1,15 @@
 import time
+with open('log.txt','r') as f:
+    a=f.readlines()
+    f.close()
+print('The last entry was\n'+a[len(a)-2])
+with open('log.txt','a+') as f:
+    f.write('\n'+(time.strftime("%H:%M:%S"))+'--'+(time.strftime("%d/%m/%Y")))
+    txts=str(input(':'))
+    see=''
+    while txts =! '':
+        see=(see+'\n'+txts)
+        print(see)
+        txts=str(input(':'))
+    f.write(see)
+    f.close()
