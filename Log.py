be420db655bb46a31c784afa3715ac42b961e263
wd7512 <35834979+wd7512@users.noreply.github.com>
2018-01-31 20:33:23 +0000

    trying to fix log

--- Log.py
+++ Log.py
@@ -1,8 +1,16 @@
 import time
 with open('log.txt','r') as f:
     a=f.readlines()
+    print(str(a))
     f.close()
-print('The last entry was\n'+a[len(a)-2])
+b=-2
+c=a[b]
+d=[]
+while c != '==========':
+    c=a[len(a)+b]
+    d.append(c)
+    b=b-1
+print('\n'.join(d[::-1]))
 with open('log.txt','a+') as f:
     f.write('\n'+(time.strftime("%H:%M:%S"))+'--'+(time.strftime("%d/%m/%Y")))
     txts=str(input(':'))
@@ -12,4 +20,5 @@ with open('log.txt','a+') as f:
         print(see)
         txts=str(input(':'))
     f.write(see)
+    f.write('\n==========')
     f.close()
